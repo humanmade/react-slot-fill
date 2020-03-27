cb7d418ea743115dce7ff5cf66571584181314d9
Ryan McCue <me@ryanmccue.info>
2020-03-27 11:52:30 +0000

    Initial commit

---
+++ src/bubbles-virtually/slot-fill-context.js
@@ -0,0 +1,15 @@
+/**
+ * WordPress dependencies
+ */
+import { createContext } from 'react';
+
+const SlotFillContext = createContext( {
+	slots: {},
+	fills: {},
+	registerSlot: () => {},
+	unregisterSlot: () => {},
+	registerFill: () => {},
+	unregisterFill: () => {},
+} );
+
+export default SlotFillContext;
